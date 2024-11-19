794c2258b8870c01ca5ccba653bdd5c34c20970e
Akreus alt <kemnitz.stephen@talentschmiede-rummelsberg.de>
2024-11-19 15:23:13 +0100

    Here are they!

---
+++ divided.py
@@ -0,0 +1,11 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a / b)
+
+print("THIS IS DIVIDING")
+a = int(input())
+print(":")
+b = int(input())
+
+print(f'{a} : {b} = {sum(a, b)}')
---
+++ plus.py
@@ -0,0 +1,12 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+print("THIS IS ADDITION")
+a = int(input())
+print("+")
+b = int(input())
+
+print(f'{a} + {b} = {sum(a, b)}')
